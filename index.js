0d8838e4e3d3f57f09c9301396b3b1071a4b341b
Jeffrey Chai <jeffrey@lavax.co>
2019-08-02 12:10:37 +0800

    Setup apollo client

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
-import { AppRegistry } from "react-native";
+import { AppRegistry, ActivityIndicator } from "react-native";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { ApolloClient, InMemoryCache, HttpLink } from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import { name as appName } from "./app.json";
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      client: null,
+      isLoading: true
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const cache = new InMemoryCache();
+      const httpLink = new HttpLink({
+        uri: "https://graphql-pokemon.now.sh/graphql"
+      });
+      const apolloClient = new ApolloClient({
+        link: httpLink,
+        cache
+      });
+      this.setState({ client: apolloClient, isLoading: false });
+    } catch (err) {}
   }
 
   render() {
-    return <AppNavigator />;
+    const { client, isLoading } = this.state;
+    if (isLoading) {
+      return <ActivityIndicator />;
+    }
+    return (
+      <ApolloProvider client={client}>
+        <AppNavigator />
+      </ApolloProvider>
+    );
   }
 }
 
